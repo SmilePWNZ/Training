0223b3e32b80ff3a9b9ed7a77b968f97f12dcbdb
Anton <bachelorpenguin@gmail.com>
2017-06-10 15:32:23 +0300

    Created a test automated script (open Amazon, search, close the browser) method: openAmazon()

--- src/test/java/testOne.java
+++ src/test/java/testOne.java
@@ -1,16 +1,15 @@
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class testOne {
-    private static By SEARCH_BAR_LOCATOR = By.id("lst-ib");
-    private static By SEARCH_BUTTON_LOCATOR = By.id("_fZl");
+    private static By SEARCH_BAR_GOOGLE_LOCATOR = By.id("lst-ib");
+    private static By SEARCH_BUTTON_GOOGLE_LOCATOR = By.id("_fZl");
+    private static By SEARCH_BAR_AMAZON_LOCATOR = By.id("twotabsearchtextbox");
+    private static By SEARCH_BUTTON_AMAZON_LOCATOR = By.xpath("//input[@type='submit']");
 
 
     private WebDriver driver;
@@ -33,9 +32,16 @@ public class testOne {
     }
 
     @Test
-    public void openGoogle() throws InterruptedException {
+    public void openGoogle() {
         driver.get("http://google.com");
-        driver.findElement(SEARCH_BAR_LOCATOR).sendKeys("Selenium WebDriver");
-        driver.findElement(SEARCH_BUTTON_LOCATOR).click();
+        driver.findElement(SEARCH_BAR_GOOGLE_LOCATOR).sendKeys("Selenium WebDriver");
+        driver.findElement(SEARCH_BUTTON_GOOGLE_LOCATOR).click();
+    }
+
+    @Test
+    public void openAmazon() {
+        driver.get("http://amazon.com");
+        driver.findElement(SEARCH_BAR_AMAZON_LOCATOR).sendKeys("Alexa");
+        driver.findElement(SEARCH_BUTTON_AMAZON_LOCATOR).click();
     }
 }
