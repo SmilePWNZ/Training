eeaf0e22dd76cdcb6799c159cb0c04699f9c5387
Anton <bachelorpenguin@gmail.com>
2017-06-17 20:36:39 +0300

    Added new classes: BaseOperations; taskFour.
    Edited login script to support "wait for element present".

--- src/test/java/BaseOperations.java
+++ src/test/java/BaseOperations.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public abstract class BaseOperations {
@@ -7,12 +9,15 @@ public abstract class BaseOperations {
     private static By USERNAME_INPUT_FIELD_LOCATOR = By.cssSelector("[data-type=text]");
     private static By PASSWORD_INPUT_FIELD_LOCATOR = By.cssSelector("[data-type=password]");
     private static By LOGIN_BUTTON_LOCATOR = By.cssSelector("[type=submit]");
+    private static By ADMIN_PAGE_LOGO_LOCATOR = By.cssSelector(".center-block.img-responsive");
 
     public static void loginToAdmin(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(USERNAME_INPUT_FIELD_LOCATOR).sendKeys("admin");
         driver.findElement(PASSWORD_INPUT_FIELD_LOCATOR).sendKeys("admin");
         driver.findElement(LOGIN_BUTTON_LOCATOR).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ADMIN_PAGE_LOGO_LOCATOR));
     }
 
 }
---
+++ src/test/java/taskFour.java
@@ -0,0 +1,78 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class taskFour extends BaseOperations {
+
+    private static By NAVIGATION_TAB_LOCATOR = By.cssSelector("[id=app-]");
+    private static By NAVIGATION_SUB_TAB_LOCATOR = By.cssSelector("[id^=doc]");
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        ChromeDriverManager.getInstance().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+        loginToAdmin(driver);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    private List<String> getNavigationTabsNames (By locator) {
+        List <WebElement> navigationTabs = driver.findElements(locator);
+        List <String> tabNames = new ArrayList<>();
+        for (WebElement element : navigationTabs) {
+            String tabName = element.getText();
+            tabNames.add(tabName);
+        }
+        return tabNames;
+    }
+
+    private By getTabByElement (String tabName) {
+        return (By.xpath("//span[contains(text(),'" + tabName + "')]"));
+    }
+
+    private void verifyAdminTabs (List<String> tabNames) {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        for (String name : tabNames) {
+            By tabElement = getTabByElement(name);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(tabElement));
+            driver.findElement(tabElement).click();
+            driver.findElement(By.cssSelector("h1"));
+            List<String> subTabNames = getNavigationTabsNames(NAVIGATION_SUB_TAB_LOCATOR);
+            for (String subName : subTabNames) {
+                By subTabElement = getTabByElement(subName);
+                wait.until(ExpectedConditions.visibilityOfElementLocated(subTabElement));
+                driver.findElement((subTabElement)).click();
+                driver.findElement(By.cssSelector("h1"));
+            }
+        }
+    }
+
+
+    @Test
+    public void navigateAdminTabs() throws InterruptedException {
+        List<String> foundTabs = getNavigationTabsNames(NAVIGATION_TAB_LOCATOR);
+        verifyAdminTabs(foundTabs);
+        }
+}
